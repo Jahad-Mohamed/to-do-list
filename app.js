d37e465b575d0915f723a6fa20eb526e9143c3c9
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-01 17:38:48 +0100

    styling added to do list elements

--- app.js
+++ app.js
@@ -18,22 +18,26 @@ function addTodo(event) {
   todoDiv.classList.add("todo");
   // Create li
   const newTodo = document.createElement("li");
-  newTodo, (innerText = "hey");
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo__tem");
   todoDiv.appendChild(newTodo);
 
   // Checkmark Button
   const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class="fas fa -check"> </i>';
+  completedButton.innerHTML = '<i class="fas fa-check"> </i>';
   completedButton.classList.add("completed-btn");
   todoDiv.appendChild(completedButton);
 
   // Bin Button
   const BinButton = document.createElement("button");
   BinButton.innerHTML = '<i class="fas fa-trash"> </i>';
-  BinButton.classList.add("completed-btn");
+  BinButton.classList.add("trash-btn");
   todoDiv.appendChild(BinButton);
 
   //APPEND TO LIST
   todoList.appendChild(todoDiv);
+
+  // Clear to do input value
+
+  todoInput.value = "";
 }
